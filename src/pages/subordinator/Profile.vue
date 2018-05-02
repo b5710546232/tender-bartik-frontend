b90fa35e99b481bebd0ac5250bbf6f872659ca42
b5710546232 <nattapat.su@ku.th>
2018-05-02 21:38:35 +0700

    add pic profile

--- src/pages/subordinator/Profile.vue
+++ src/pages/subordinator/Profile.vue
@@ -30,8 +30,8 @@
           </div>
         </div>
   
-        <div class="col-md-7 col-sm-12 history-wrapper card">
-          <div class="row history">
+        <div class="col-md-7 col-sm-12 history-wrapper">
+          <div class="row history card">
             <div class="col-md-12">
               <h2>History</h2>
             </div>
@@ -95,7 +95,7 @@
   export default {
     data() {
       return {
-        imgSrc: "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20200%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1626e042cce%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1626e042cce%22%3E%3Crect%20width%3D%22200%22%20height%3D%22200%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2274.4296875%22%20y%3D%22104.65%22%3E200x200%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
+        imgSrc: "../../../static/assets/img/blank_profile.png",
         myTasks: [],
         myLeaves: [{
           start:"...",
@@ -131,10 +131,17 @@
     },
     methods:{
       getImage(){
-        if(this.userProfile.image_path){
+        const checkIsImage = (image_path)=>{
+          if(String(image_path).substr(0,7)==="storage"){
+            return true
+          }
+          return false
+        }
+        // console.log("checkIsImage",checkIsImage(this.userProfile.image_path ))
+        if(this.userProfile.image_path && checkIsImage(this.userProfile.image_path )){
           return 'https://limitless-falls-39048.herokuapp.com/'+this.userProfile.image_path
         }
-        return this.imgSrc
+        return "../../../static/assets/img/blank_profile.png"
       }
     }
   };
