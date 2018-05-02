40255e277865e888ad370fe77c6dc96fe69a3aca
kasiditp <kasidit.p@ku.th>
2018-05-02 21:41:08 +0700

    partial commit

--- src/pages/supervisor/LeaveHistory.vue
+++ src/pages/supervisor/LeaveHistory.vue
@@ -11,39 +11,12 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-          </tr>
-          <tr>
-            <th scope="row">4</th>
-            <td>Mark</td>
-          </tr>
-          <tr>
-            <th scope="row">5</th>
-            <td>Mark</td>
+          <tr v-for="leave in myLeaves" :key="leave.id">
+            <th scope="row">{{ leave.start }}</th>
+            <td>{{ leave.substitution_id }}</td>
           </tr>
         </tbody>
       </table>
-    </div>
-    <div class="row">
-        <nav aria-label="Page navigation example" style="margin: 0 auto;">
-          <ul class="pagination">
-            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item"><a class="page-link" href="#">Next</a></li>
-          </ul>
-        </nav>
     </div>
   </div>
 </template>
